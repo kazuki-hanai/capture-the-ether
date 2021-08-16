219b222dd52aae984ad657b5d53710518c1c0e3c
wan-nyan-wan <distributed.system.love@gmail.com>
2021-08-17 01:56:56 +0900

    complete guess the number!

--- src/typescript/guess_the_number.ts
+++ src/typescript/guess_the_number.ts
@@ -1,3 +1,4 @@
+import { exit } from 'process';
 import Web3 from 'web3';
 require('dotenv').config();
 
@@ -6,13 +7,12 @@ const solution = async () => {
 
     const account = web3.eth.accounts.privateKeyToAccount(process.env.ETH_ACCOUNT || 'invalid');
 
-    const abi = require('../../abi/choose_a_nickname.json');
-
+    const abi = require('../../abi/guess_the_number.json');
     const contract = new web3.eth.Contract(abi, process.env.CONTRACT_ADDR || 'invalid');
 
     const networkId = await web3.eth.net.getId();
-    const tx = contract.methods.setNickname(web3.utils.asciiToHex("CynicalCat"));
-    const gas = await tx.estimateGas({from: account.address});
+    const tx = contract.methods.guess(42);
+    const gas = await tx.estimateGas({from: account.address, value: web3.utils.toWei('1', 'ether')});
     const gasPrice = await web3.eth.getGasPrice();
     const data = tx.encodeABI();
     const nonce = await web3.eth.getTransactionCount(account.address);
@@ -22,12 +22,18 @@ const solution = async () => {
             to: contract.options.address, 
             data,
             gas,
+            value: web3.utils.toWei('1', 'ether'),
             gasPrice,
             nonce, 
             chainId: networkId
         },
         account.privateKey
-    );
+    ).catch(err => {
+        console.log('signTransaction');
+        console.log(err);
+        exit(1);
+    });
+
     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction || '');
     console.log(`Transaction hash: ${receipt.transactionHash}`);
 }
