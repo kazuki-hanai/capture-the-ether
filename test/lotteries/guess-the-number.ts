ad465805ef22c9dff5a80823c96388e60e726153
wan-nyan-wan <distributed.system.love@gmail.com>
2021-10-17 02:35:28 +0900

    complete guess-the-number

--- test/lotteries/guess-the-number.ts
+++ test/lotteries/guess-the-number.ts
@@ -1,43 +1,20 @@
-import { exit } from 'process';
-import Web3 from 'web3';
-require('dotenv').config();
+import { ethers } from "hardhat";
+import { Contract, Signer } from "ethers";
+import { expect } from "chai";
 
-const solution = async () => {
-    const web3 = new Web3(process.env.WEB3_PROVIDER || 'invalid');
+let contract: Contract; // challenge contract
 
-    const account = web3.eth.accounts.privateKeyToAccount(process.env.ETH_ACCOUNT || 'invalid');
+before(async () => {
+  const factory = await ethers.getContractFactory("GuessTheNumberChallenge");
+  contract = factory.attach(`0x26d106d839d2C6f083264820B485899bb6889a6a`);
+});
 
-    const abi = require('../../abi/guess_the_number.json');
-    const contract = new web3.eth.Contract(abi, );
+it("solves the challenge", async function () {
+  const tx = await contract.guess(42, {
+    value: ethers.utils.parseEther(`1`),
+    gasLimit: 1e5,
+  });
+  const txHash = tx && tx.hash;
+  expect(txHash).to.not.be.undefined;
+});
 
-    const networkId = await web3.eth.net.getId();
-    const tx = contract.methods.guess(42);
-    const gas = await tx.estimateGas({from: account.address, value: web3.utils.toWei('1', 'ether')});
-    const gasPrice = await web3.eth.getGasPrice();
-    const data = tx.encodeABI();
-    const nonce = await web3.eth.getTransactionCount(account.address);
-
-    const signedTx = await web3.eth.accounts.signTransaction(
-        {
-            to: contract.options.address, 
-            data,
-            gas,
-            value: web3.utils.toWei('1', 'ether'),
-            gasPrice,
-            nonce, 
-            chainId: networkId
-        },
-        account.privateKey
-    ).catch(err => {
-        console.log('signTransaction');
-        console.log(err);
-        exit(1);
-    });
-
-    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction || '');
-    console.log(`Transaction hash: ${receipt.transactionHash}`);
-}
-
-solution()
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
