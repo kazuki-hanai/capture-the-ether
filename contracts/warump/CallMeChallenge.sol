5bf34c403f8e52750df06826812b44d1564f2c44
wan-nyan-wan <distributed.system.love@gmail.com>
2021-10-17 02:02:52 +0900

    fix folder structure

---
+++ contracts/warump/CallMeChallenge.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.21;
+
+contract CallMeChallenge {
+    bool public isComplete = false;
+
+    function callme() public {
+        isComplete = true;
+    }
+}
+
---
+++ test/warmup/call-me.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+import { Contract, Signer } from "ethers";
+import { expect } from "chai";
+
+let accounts: Signer[];
+let eoa: Signer;
+let contract: Contract;
+
+before(async () => {
+  accounts = await ethers.getSigners();
+  eoa = accounts[0];
+  // contract = new ethers.Contract(
+  //   `0x7e53cBe1AE1D8BCc1e4273ED31eb61bC4513C509`,
+  //   ``,
+  //   accounts[0]
+  // );
+  const factory = await ethers.getContractFactory("CallMeChallenge");
+  contract = factory.attach(`0x7e53cBe1AE1D8BCc1e4273ED31eb61bC4513C509`);
+});
+
+it("solves the challenge", async function () {
+  const tx = await contract.callme();
+  const txHash = tx.hash;
+  expect(txHash).to.not.be.undefined;
+});
